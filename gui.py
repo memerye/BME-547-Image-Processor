444784892f6b7ff236fa45b8bb2d04a93bdf8e07
Lucy Liang <lucy.liang@duke.edu>
2019-12-05 08:53:18 -0500

    created gui.py for GUI code

---
+++ gui.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter import ttk
+
+
+# User login/create account
+def login_window():
+
+    root = Tk()
+    root.title('Create Account/User Login')
+
+    root.mainloop()
+    return
+
+
+if __name__ == '__main__':
+    login_window()
