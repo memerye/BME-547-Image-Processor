f34fe061d1a63029ed88567fdc9a5d7d2aa369fe
memerye <lx66@duke.edu>
2019-12-07 17:09:37 -0500

    added test for function add_new_user_to_db

---
+++ test_initial_database.py
@@ -0,0 +1,16 @@
+# test_initial_database.py
+import pytest
+from pymodm import connect
+
+
+@pytest.mark.parametrize("user_info, u_id", [
+    ({"user_id": "Bob1"}, "Bob1"),
+    ({"user_id": "Jenny2"}, "Jenny2")
+])
+def test_add_new_user_to_db(user_info, u_id):
+    from initial_database import add_new_user_to_db, ImageUser
+    connect("mongodb+srv://python_code:bme547final@bme547-reiux."
+            "mongodb.net/test?retryWrites=true&w=majority")
+    add_new_user_to_db(user_info)
+    u = ImageUser.objects.raw({"_id": u_id}).first()
+    assert u_id == u.user_id
