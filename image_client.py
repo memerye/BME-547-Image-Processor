7f799a15ee0c39cca9a92158a39eebc516e5294a
memerye <lx66@duke.edu>
2019-12-08 18:59:23 -0500

    add more details to the docstring

--- image_client.py
+++ image_client.py
@@ -8,7 +8,7 @@ def post_user_id(info):
     """The client function of posting a new user.
 
     Args:
-        info (dict): the dictionary of patient's information.
+        info (dict): the dictionary of user's information.
 
     Returns:
         None
@@ -18,11 +18,18 @@ def post_user_id(info):
     print(r)
     print(r.text)
     print(r.status_code)
+    return None
 
 
 def test_add_patients():
-    """Create a pseudo-list of patients information to
-    add into the server and database.
+    """Create pseudo-lists of user information to add into the
+    server and database.
+
+    The pseudo-lists of users contain errors of:
+    (1) Wrong dictionary key
+    (2) Wrong format of id
+    Ultimately three valid patient information will be added into
+    the database with the id of "1_a", "08563", "Lily121".
 
     Returns:
         None
@@ -38,6 +45,16 @@ def test_add_patients():
 
 
 def request_check_id():
+    """The client function of checking the existence of this id.
+
+    The pseudo-lists of ids are expected as:
+    * "whoisthis" doesn't exist in tha database
+    * "1_a", "08563", and "Lily121" have already registered into the database
+    The results will be printed out.
+
+    Returns:
+        None
+    """
     ids = ["1_a", "08563", "Lily121", "whoisthis"]
     for p_id in ids:
         r = requests.get("http://127.0.0.1:5000/api/check_id/{}".format(p_id))
@@ -46,6 +63,7 @@ def request_check_id():
         if r.status_code == 200:
             answer = r.json()
             print(answer)
+    return None
 
 
 if __name__ == '__main__':
