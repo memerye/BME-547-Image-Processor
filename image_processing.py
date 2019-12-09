4e114076bc00c86a71f89eeffdc85ab747ad488c
memerye <lx66@duke.edu>
2019-12-08 21:33:19 -0500

    histagram equalization in class

--- image_processing.py
+++ image_processing.py
@@ -18,3 +18,18 @@ def convert_image_to_uint8(array):
         return result.astype(np.uint8)
     else:
         return array.astype(np.uint8)
+
+
+class ImageProcessing(object):
+    def __init__(self, image):
+        self.image = convert_image_to_uint8(image)
+        self.shape = image.shape
+
+    def __len__(self):
+        return len(self.image)
+
+    def histeq(self):
+        start = time()
+        img_eq = exposure.equalize_hist(self.image)
+        img_eq_uint8 = convert_image_to_uint8(img_eq)
+        return img_eq_uint8, time()-start
