e1063ce2d7bc44f20daf61bb07c689f4886c663b
Lucy Liang <lucy.liang@duke.edu>
2019-12-10 00:04:13 -0500

    added functions request_check_id to check if user exists  and post_user_id to create account for new user

--- GUI_client.py
+++ GUI_client.py
@@ -1,7 +1,21 @@
 import requests
+from skimage import io
+from en_de_code import image_to_b64, b64_to_image
+import os
 
 
-def validate_user(x):
-    r = requests.post("##", json=x)
+def request_check_id(x):
+    r = requests.get("http://127.0.0.1:5000/api/check_id/{}".format(x))
     answer = r.json()
-    return answer
+    # print(answer["result"])
+    return answer["result"]
+
+
+def post_user_id(info):
+    r = requests.post("http://127.0.0.1:5000/api/new_user",
+                      json=info)
+    return None
+
+
+if __name__ == '__main__':
+    request_check_id()
