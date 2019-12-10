4699e7b44f029e958754686ae81d253f9b9870e1
Lucy Liang <lucy.liang@duke.edu>
2019-12-10 03:05:08 -0500

    added function request_history_info to retrieve user processing history

--- GUI_client.py
+++ GUI_client.py
@@ -23,5 +23,12 @@ def request_user_info(ids):
     return answer
 
 
+def request_history_info(ids):
+    r = requests.get("http://127.0.0.1:5000/api/history_info/{}"
+                     .format(ids))
+    answer = r.json()
+    return answer
+
+
 if __name__ == '__main__':
     request_check_id()
