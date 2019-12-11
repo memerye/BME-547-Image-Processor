23917b80b6d9d924f27a66190e3655b7ba4d0fd5
Lucy Liang <lucy.liang@duke.edu>
2019-12-11 03:30:05 -0500

    added functions post_process_opt, request_one_history_info, request_recent_process_images to post and retrieve info for image display

--- GUI_client.py
+++ GUI_client.py
@@ -4,8 +4,8 @@ from en_de_code import image_to_b64, b64_to_image
 import os
 
 
-def request_check_id(x):
-    r = requests.get("http://127.0.0.1:5000/api/check_id/{}".format(x))
+def request_check_id(ids):
+    r = requests.get("http://127.0.0.1:5000/api/check_id/{}".format(ids))
     answer = r.json()
     print(answer["result"])
     return answer["result"]
@@ -23,10 +23,25 @@ def request_user_info(ids):
     return answer
 
 
+def post_process_opt(info):
+    """The client function of posting processed images.
+
+    Args:
+        info (dict): The dictionary of image information.
+
+    Returns:
+        None
+    """
+    r = requests.post("http://127.0.0.1:5000/api/process",
+                      json=info)
+    return None
+
+
 def request_history_info(ids):
     r = requests.get("http://127.0.0.1:5000/api/history_info/{}"
                      .format(ids))
     answer = r.json()
+    print(answer)
     return answer
 
 
@@ -34,8 +49,35 @@ def request_one_history_info(ids, num):
     r = requests.get("http://127.0.0.1:5000/api/history_info/{}/{}"
                      .format(ids, num))
     answer = r.json()
+    print(answer)
+    return answer
+
+
+def request_recent_process_images(ids):
+    """The client function of processing the images that just uploaded
+
+     User information includes:
+     (1) user id
+     (2) uploaded time of the image(s)
+     (3) the operation name
+     (4) the size of the image(s)
+     (5) the CPU running time for each image processing
+     (6) the image name(s)
+     (7) the raw image(s)
+     (8) the processed image(s)
+     While user "08563" is not in the database,
+     and we can get the expected message from server.
+
+     The results will be printed out.
+
+     Returns:
+         None
+     """
+    r = requests.get("http://127.0.0.1:5000/api/"
+                     "most_recent_processed_image/{}".format(ids))
+    answer = r.json()
     return answer
 
 
 if __name__ == '__main__':
-    request_one_history_info('1_a', '1')
+    request_history_info('1_a')
